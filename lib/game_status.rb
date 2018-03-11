bfb142596d7af00e254726da448ee77abc85e26b
Precious Humphrey <phumphrey23@gmail.com>
2018-03-11 21:31:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,7 +62,7 @@ def winner(board)
     return "X"
  elsif board[won?(board)[0]] == "O"
    return "O"
- else !full?(board) || draw(board)
+ else (draw?(board) || !full?(board)) && !won?(board)
    return nil
  end
 end
