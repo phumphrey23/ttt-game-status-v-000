a095b09e55b942b486fe06f20cb096ed2aecc662
Precious Humphrey <phumphrey23@gmail.com>
2018-03-11 20:56:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,23 +23,15 @@ end
 
 def won?(board)
 
-  WIN_COMBINATIONS.detect do |win_combination|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  WIN_COMBINATIONS.each do |win_combination|
+  if win_combination.all?{|y| board[y] == "X"}
     return win_combination
-  elsif if position_1 == "O" && position_2 == "O" && position_3 == "O"
+  elsif if win_combination.all?{|y| board[y] == "O"}
     return win_combination
-  else
-    false
+  else board.none? { |i| i == "X" || i = "O"}
+    return false
    end
-
+ end
 
 
 
